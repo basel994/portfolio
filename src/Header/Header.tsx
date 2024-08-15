3fb95f0b159fab21a7da4fe3d3bf5a7763548a13
baselbalkes <baselbalkes77@gmail.com>
2024-08-15 11:09:10 +0300

    add navbar

---
+++ src/Header/Header.tsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <p className="logo">Basel Balkees</p>
+            <ul className="menu">
+                <li>الرئيسية</li>
+                <li>تسجيل الدخول</li>
+                <li>التسجيل</li>
+                <li>حول</li>
+            </ul>
+        </div>
+    )
+}
